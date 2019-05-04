6d89cbcea32c0520a28a55167862d23ac1f507c8
Stephane Ehu <stephaneehu@gmail.com>
2019-05-04 23:06:02 +0530

    Fifth Commit - Adding SonarQube Comment to test QA-update

--- src/main/java/ci/bda/confirming/model/Tickets.java
+++ src/main/java/ci/bda/confirming/model/Tickets.java
@@ -4,6 +4,9 @@ public class Tickets {
  private long id;
  private String description;
 
+public Tickets(){
+	
+}
 
 public Long getId(){
 	return this.id;
@@ -17,9 +20,18 @@ public String getDescription(){
 	return this.description;
 }
 
-public void setDescription(String description){
+
+  public void setDescription(String description){
 this.description=description;
 }
 
+
 }
 //e295ddd889fed7ad4cbeaa752e83ca825d0f2aa8
+/*
+mvn sonar:sonar \
+  -Dsonar.projectKey=stephanehu_confirming \
+  -Dsonar.organization=stephanehu-github \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=e295ddd889fed7ad4cbeaa752e83ca825d0f2aa8
+  */
