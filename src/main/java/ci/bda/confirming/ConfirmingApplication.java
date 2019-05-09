5b9b566afac830b520dbd2d2d9027c8ea8436c42
Stephane Ehu <stephaneehu@gmail.com>
2019-05-09 19:26:29 +0530

    Another comment

--- src/main/java/ci/bda/confirming/ConfirmingApplication.java
+++ src/main/java/ci/bda/confirming/ConfirmingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConfirmingApplication {
-
+//Other Comment
 	public static void main(String[] args) {
 		SpringApplication.run(ConfirmingApplication.class, args);
 	}
